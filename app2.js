c868249b4f1c9d1bdd9c1f5c9574a879c484a6a0
Simon Gerula <saimonsir@gmail.com>
2020-11-20 10:36:42 -0300

    prueba js 7.1

--- app2.js
+++ app2.js
@@ -46,7 +46,7 @@ function loadJobs() {
     .then(response => response.json())
     .then(data => {
         dataJobs = data;
-        numberOfPages = (Math.ceil(dataJobs / JobsPerPage));
+        numberOfPages = (Math.ceil(dataJobs / jobsPerPage));
         createPage();
     });
 
